df4c88b8ecf5b3a81f1396c134a6198183c14062
Andres Cassanaz <andres.cassanaz@redb.ee>
2019-11-08 13:40:33 -0300

    Agregada lista de favoritos guardadas

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
@@ -15,13 +14,11 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWords extends StatefulWidget {
-
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
-
   final List<WordPair> _suggestion = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final Set<WordPair> _saved = Set<WordPair>();
@@ -29,20 +26,54 @@ class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup name generator')
+        appBar: AppBar(
+          title: Text('Startup name generator'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+          ],
+        ),
+        body: _buildSuggestions());
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );                       // ... to here.
+        },
       ),
-    body: _buildSuggestions()
     );
   }
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
+      itemBuilder: (context, i) {
         if (i.isOdd) return Divider();
         final index = i ~/ 2;
-        if(index >= _suggestion.length){
+        if (index >= _suggestion.length) {
           _suggestion.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestion[index]);
@@ -50,13 +81,10 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
+      title: Text(pair.asPascalCase, style: _biggerFont),
       trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.blue : null,
